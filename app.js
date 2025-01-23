88c71b4776bb447c67f633a0454aeb3ca9f4f842
Tucker <etucker2@una.edu>
2025-01-23 16:27:44 -0600

    class time

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+const bodyParser = require("body-parser")
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
@@ -19,4 +21,22 @@ app.get('/helloRender', function (req, res) {
   res.send('Hello Express from Real World<br><a href="/">back to home</a>')
 })
 
-app.listen(port, ()=> console.log (`server is running on ... ${port}`));
+app.get('/saveMyName', (req, res)=>{
+  console.log('Did we hit the get endpoint?');
+
+  console.log(req.query);
+
+  res.redirect('/ejs');
+})
+
+app.post('/saveMyName', (req, res)=>{
+  console.log('Did we hit the post endpoint?');
+
+  console.log(req.body);
+
+  //res.redirect('/ejs');
+
+  res.render('words', {pageTitle: req.body.myName});
+})
+
+app.listen(port, ()=> console.log (`server is running on ... local host:${port}`));
