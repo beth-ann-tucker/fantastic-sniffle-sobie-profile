23b9083b63afe1c3b97d13a1a60a50d80e95d128
Tucker <etucker2@una.edu>
2025-02-11 16:28:00 -0600

    finally caught up

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const {ObjectId} = require('mongodb');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGO_URI;
 
-// console.log(uri);
-
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'))
@@ -23,117 +21,68 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-
 //run().catch(console.dir);
 
-async function getData(){
+const mongoCollection = client.db("bathAnnSobie-profile-database").collection("bethAnnBlog");
 
-  await client.connect();
-  let collection = await client.db("sobie-profile-database").collection("sobie-profile-data");
-
-  let results = await collection.find({}).toArray();
-    //.limit(50)
-    //.toArray();
-  
-  console.log(results)
-
-  return results;
-
+function initProfileData(){
+    mongoCollection.insertOne({
+      title: "this is the blog title",
+      post: "this is the post"
+    });
 }
 
-app.get('/read', async function (req, res){
+// initProfileData();
 
+app.get('/', async function (req, res){
+  
+  let results = await mongoCollection.find({}).toArray();
 
+  res.render('profile', 
+    {profileData : results} ); 
 
-  let getDataResults = await getData();
-  console.log(getDataResults);
-  res.render('names', { nameData: getDataResults});
 })
 
 app.post('/insert', async function (req, res){
 
-  console.log('in /insert');
 
-  let newName = req.body.myName;
+  let results = await mongoCollection.insertOne({
+      title: req.body.title,
+      post: req.body.post
 
-  await client.connect();
-
-  await client.db("sobie-profile-database").collection("sobie-profile-data").insertOne({name: newName});
-
-  res.redirect('/read');
-})
-
-app.post('/delete/:id', async function (req, res){
-
-  console.log('in delete, req.parms.id: ', req.params.id);
-
-  client.connect();
-
-  const collection =  client.db("sobie-profile-database").collection("sobie-profile-data").insertOne({name: newName});
-
-  let result = await collection.findOneAndDelete(
-    {
-      "_id": new ObjectId(req.params.id)
-    }
-  ).then(result => );
-
-  res.redirect('/read');
-})
-
-
-
-getData();
-
-
-//begin all my middlewears
-
-app.get('/', function (req, res) {
-  res.sendFile('index.html');
-})
-
-app.get('/ejs', function (req, res) {
-  res.render('words', {
-    pageTitle: "my ejs page"
   });
-})
 
-app.get('/helloRender', function (req, res) {
-  res.send('Hello Express from Real World<br><a href="/">back to home</a>')
-})
-
-app.get('/saveMyName', (req, res)=>{
-  console.log('Did we hit the get endpoint?');
-
-  console.log('req.query: ', req.query);
-
-  console.log('req.query: ', req.params);
-
-  let reqName = req.query.myNameGet;
-
-  res.render('words', {pageTitle: reqName});
+  res.redirect('/');
 })
 
 
-app.post('/saveMyName', (req, res)=>{
-  console.log('Did we hit the post endpoint?');
-
-  console.log(req.body);
-
-  //res.redirect('/ejs');
-
-  res.render('words', {pageTitle: req.body.myName});
+app.post('/update', async (req,res)=>{
+  let result = await mongoCollection.findOneAndUpdate( 
+  {_id: ObjectId.createFromHexString(req.body.updateId)}, { 
+    $set: 
+      {
+        title : req.body.updateTitle, 
+        post : req.body.updatePost 
+      }
+     }
+  ).then(result => {
+  console.log(result); 
+  res.redirect('/');
 })
+}); 
 
-app.listen(port, ()=> console.log (`server is running on ... local host:${port}`));
+app.post('/delete', async function (req, res){
+
+  let result = await mongoCollection.findOneAndDelete( 
+    {
+      "_id": new ObjectId(req.body.deleteId)
+    }
+  ).then(result => {
+    
+    res.redirect('/');
+  })
+
+}); 
+
+
+app.listen(port, ()=> console.log(`server is running on ... localhost:${port}`) );
